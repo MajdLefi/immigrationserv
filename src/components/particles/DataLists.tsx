65b039b0175b5b54007b389e3ad3c7e7ce642359
MajdLefi <majd.lefi@esprit.tn>
2023-08-16 23:28:09 +0100

    first commit

---
+++ src/components/particles/DataLists.tsx
@@ -0,0 +1,237 @@
+export const NavLinks = [
+    {
+        name: "Destination",
+        url: "/"
+    },
+    {
+        name: "Hotels",
+        url: "/"
+    },
+    {
+        name: "Flights",
+        url: "/"
+    },
+    {
+        name: "Bookings",
+        url: "/"
+    }
+
+];
+
+export const NavButtons = [
+    {
+        name: "Login",
+        url: "/"
+    },
+    {
+        name: "Signup",
+        url: "/"
+    }
+
+];
+
+
+export const HeroTexts = {
+    firstText: "Tracer votre chemin vers un avenir meilleur",
+    secondText: "CVs d'Excellence et Soutien à l'Immigration",
+    thirdText: "Bienvenue dans notre monde dédié à vous aider à réaliser vos rêves d'immigration. Avec nos CVs d'excellence et notre accompagnement personnalisé, nous trions les défis du processus d'immigration pour que votre voyage vers de nouveaux horizons soit aussi fluide que possible. Rejoignez-nous aujourd'hui et commencez à créer votre avenir dans un nouvel environnement.",
+    firstButton: "Find out more",
+    secondButton: 'Play Demo'
+}
+
+
+export const ServiceTexts = {
+    firstText: "Qualité garantie !",
+    secondText: "Nous offrons les meilleurs services.",
+    cards: [
+        {
+            firstText: "Sur mesure",
+            secondText: "Création de CV sur Mesure"
+        },
+        {
+            firstText: "Révision",
+            secondText: "Révision Professionnelle du CV"
+        },
+        {
+            firstText: "Postuler",
+            secondText: "Inscription sur les plateformes d'immigration"
+        },
+        {
+            firstText: "Préparation",
+            secondText: "Préparation aux entretiens"
+        }
+    ]
+}
+
+export const TopDestinationTexts = {
+    firstText: "Destinations de Carrière Privilégiées",
+    secondText: "Top Destinations",
+    cards: [
+        {
+            country: "Canada, Montréal",
+            price: "5.42k",
+            duration: "10 days trip"
+        },
+        {
+            country: "Canada, Québéc",
+            price: "5.42k",
+            duration: "10 days trip"
+        },
+        {
+            country: "KSA",
+            price: "4.5k",
+            duration: "8 days trip"
+        },
+        {
+            country: "Quatar",
+            price: "3.9k",
+            duration: "14 days trip"
+        },
+        {
+            country: "London, UK",
+            price: "4.2k",
+            duration: "12 days trip"
+        },
+        {
+            country: "Paris, France",
+            price: "15k",
+            duration: "26 days trip"
+        },
+    ]
+}
+
+export const BookingStepsTexts = {
+    firstText: "Facile et Rapide",
+    secondText: "Réservez votre prochain voyage en 3 étapes simples",
+    listOfSteps: [
+        {
+            text: "Choisissez votre destination"
+        },
+        {
+            text: "Envoyer vos informations et coordonnées (vos données sont en toute sécurite)"
+        },
+        {
+            text: "La préparation des cv's et demandes avec assistance"
+        }
+    ],
+    cardOne:
+    {
+        name: "Service rapide !",
+        date: "",
+        people: "24 people going"
+    },
+    cardTwo: {
+        status: "Ongoing",
+        destination: "Départ",
+        completion: "40% Completed"
+    }
+
+}
+
+export const TestimonialTexts = {
+    firstText: "TESTIMONIALS",
+    secondText: "What people say about Us.",
+    feedBacks: [
+        {
+            text: "You did a fabulous job. There were no hitches. Thank you again for all your work planning this trip.",
+            person: "Mike taylor",
+            location: "Lahore, Pakistan"
+        },
+        {
+            text: "Jadoo was great with the entire process from planning to updates during the trip. We had 11 people and everything was perfectly executed. We appreciate all of her hard work. It was truly the trip of a lifetime. Thank you!",
+            person: "Gina",
+            location: "Owerri, Nigeria"
+        },
+        {
+            text: "Booking through you was very easy and made our lives so much easier. I have nothing bad to say! Thank you for giving us tips and guidance before we left on what to bring and such, that was very helpful!",
+            person: "Emma",
+            location: "Vancouver, Canada"
+        },
+        {
+            text: "Thank you for your recommendation and putting the trip together. The resort was absolutely beautiful. The infinity pools, the palm trees in the main pool, the infinity pool in out preferred area overlooking the gulf and the golf course were exceptional....",
+            person: "Loveth",
+            location: "Lagos, Nigeria"
+        },
+    ],
+}
+
+export const NewsletterTexts = {
+    firstText: "Subscribe to get information, latest news and other interesting offers about Jadoo.",
+    placeholderText: "Your email",
+    buttonText: "Subscribe"
+}
+
+export const FooterTexts = {
+    underLogoText: "Book your trip in minutes, get full control for much longer.",
+    quickLinks: {
+        caption: "Quick Links",
+        links: [
+            {
+                name: "Destination",
+                url: "/"
+            },
+            {
+                name: "Hotels",
+                url: "/"
+            },
+            {
+                name: "Flights",
+                url: "/"
+            },
+            {
+                name: "Bookings",
+                url: "/"
+            },
+            {
+                name: "Login",
+                url: "/"
+            },
+            {
+                name: "Signup",
+                url: "/"
+            }
+        ]
+    },
+    contacts: {
+        caption: "Contacts",
+        links: [
+            {
+                name: "FAQs",
+                url: "/"
+            },
+            {
+                name: "Help",
+                url: "/"
+            },
+            {
+                name: "Policies",
+                url: "/"
+            },
+            {
+                name: "Terms & Conditions",
+                url: "/"
+            }
+        ]
+    },
+    more: {
+        caption: "More",
+        links: [
+            {
+                name: "Career",
+                url: "/"
+            },
+            {
+                name: "Airlines",
+                url: "/"
+            },
+            {
+                name: "Airline Fees",
+                url: "/"
+            },
+            {
+                name: "Low Fare Tips",
+                url: "/"
+            }
+        ]
+    }
+}
